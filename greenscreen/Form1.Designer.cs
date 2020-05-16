bbebb914970b3ed7efdc394b82787ed0b25202d4
erikerik771 <55601424+erikerik771@users.noreply.github.com>
2020-05-16 16:03:48 +0300

    Add files via upload

---
+++ greenscreen/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace greenscreen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnSet = new System.Windows.Forms.Button();
+            this.btnBr = new System.Windows.Forms.Button();
+            this.btnbr1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(20, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(343, 360);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(369, 42);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(343, 360);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(718, 42);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(343, 360);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btnSet
+            // 
+            this.btnSet.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnSet.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnSet.Location = new System.Drawing.Point(807, 408);
+            this.btnSet.Name = "btnSet";
+            this.btnSet.Size = new System.Drawing.Size(165, 42);
+            this.btnSet.TabIndex = 3;
+            this.btnSet.Text = "Begin!";
+            this.btnSet.UseVisualStyleBackColor = false;
+            this.btnSet.Click += new System.EventHandler(this.btnSet_Click);
+            // 
+            // btnBr
+            // 
+            this.btnBr.Location = new System.Drawing.Point(443, 408);
+            this.btnBr.Name = "btnBr";
+            this.btnBr.Size = new System.Drawing.Size(165, 42);
+            this.btnBr.TabIndex = 4;
+            this.btnBr.Text = "Imaginea 2";
+            this.btnBr.UseVisualStyleBackColor = true;
+            this.btnBr.Click += new System.EventHandler(this.btnBr_Click);
+            // 
+            // btnbr1
+            // 
+            this.btnbr1.Location = new System.Drawing.Point(98, 408);
+            this.btnbr1.Name = "btnbr1";
+            this.btnbr1.Size = new System.Drawing.Size(165, 42);
+            this.btnbr1.TabIndex = 5;
+            this.btnbr1.Text = "Imaginea 1";
+            this.btnbr1.UseVisualStyleBackColor = true;
+            this.btnbr1.Click += new System.EventHandler(this.btnbr1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(373, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(329, 31);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "GreenScreen/Chroma key";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 459);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnbr1);
+            this.Controls.Add(this.btnBr);
+            this.Controls.Add(this.btnSet);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnSet;
+        private System.Windows.Forms.Button btnBr;
+        private System.Windows.Forms.Button btnbr1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ greenscreen/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace greenscreen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSet_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp1 = (Bitmap)pictureBox1.Image.Clone();
+            Bitmap bmpRs = bmp1;
+            Bitmap bmp2 =(Bitmap)pictureBox2.Image;
+            Graphics g = Graphics.FromImage(bmpRs);
+            Bitmap bmptr = new Bitmap(bmp2.Width, bmp2.Height);
+            for (int y = 0; y < bmptr.Height; y++)
+            {
+                // ...and from left to right
+                for (int x = 0; x < bmptr.Width; x++)
+                {
+                    // Determine the pixel color
+                    Color camColor = bmp2.GetPixel(x, y);
+
+                    // Every component (red, green, and blue) can have a value from 0 to 255, so determine the extremes
+                    byte max = Math.Max(Math.Max(camColor.R, camColor.G), camColor.B);
+                    byte min = Math.Min(Math.Min(camColor.R, camColor.G), camColor.B);
+
+                    // Should the pixel be masked/replaced?
+                    bool replace =
+                        camColor.G != min // verde este cea mai mica valoare
+                        && (camColor.G == max // green is the biggest 
+                        || max - camColor.G < 8) // or at least almost the biggest value
+                        && (max - min) > 26; // minimum difference between smallest/biggest value (avoid grays)
+
+                    if (replace)
+                        camColor = Color.Transparent;
+
+                    // Set the output pixel
+                    bmptr.SetPixel(x, y, camColor);
+                }
+            }
+            g.DrawImage(bmptr, 0, 0, bmp1.Size.Width, bmp1.Size.Height);
+            pictureBox3.Image = bmpRs;
+        }
+
+        //browse images
+
+        private void btnBr_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if(dlg.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox2.Image = System.Drawing.Image.FromFile(dlg.FileName);
+            }    
+        }
+        private void btnbr1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = System.Drawing.Image.FromFile(dlg.FileName);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
